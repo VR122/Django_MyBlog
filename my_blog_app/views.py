ff7da1e83bc3e382ad9b3100b1497954493e3586
Vaibhav Reddy <vaibhavreddy0852@gmail.com>
2025-02-04 08:21:40 +0530

    added comment model and added styling. Changed the views file to classic standard View

--- my_blog_app/views.py
+++ my_blog_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.views.generic import ListView
+from django.views import View
+from django.urls import reverse
 from .models import Post
+from .form import CommentForm
 # Create your views here.
 
 
@@ -19,11 +22,18 @@ class AllPostsView(ListView):
     context_object_name = "posts"
 
 
-class PostDetailView(DetailView):
-    template_name = "my_blog/post_detail.html"
-    model = Post
+class PostDetailView(View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tags'] = self.object.tags.all()
-        return context
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+        return render(request, "my_blog/post_detail.html", {"post": post, "tags":post.tags.all(), "comment_form": CommentForm()})
+    
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            return HttpResponseRedirect(reverse("post_detail", args=[slug]))
+        return render(request, "my_blog/post_detail.html", {"post": post, "comment_form": comment_form})
