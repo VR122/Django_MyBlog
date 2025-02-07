bb4d4f059f7249325109b7a98180bb3c006beb42
Vaibhav Reddy <vaibhavreddy0852@gmail.com>
2025-02-07 07:50:16 +0530

    ordered comments by date

--- my_blog_app/views.py
+++ my_blog_app/views.py
@@ -30,7 +30,7 @@ class PostDetailView(View):
                    "post": post, 
                    "tags":post.tags.all(), 
                    "comment_form": CommentForm(), 
-                   "comments": post.comments.all()
+                   "comments": post.comments.all().order_by("-date")
                    }
         return render(request, "my_blog/post_detail.html", context)
     
@@ -45,6 +45,6 @@ class PostDetailView(View):
         context = {
             "post": post, 
             "comment_form": comment_form, 
-            "comments": post.comments.all()
+            "comments": post.comments.all().order_by("-date")
             }
         return render(request, "my_blog/post_detail.html", context)
